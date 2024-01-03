1b63521e1f88cac267c33f4f05621137110c18d0
Mario Piccitto <mario.piccitto2@external.nexigroup.com>
2024-01-03 18:07:12 +0100

    header component

--- exercise/src/app/app.module.ts
+++ exercise/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
---
+++ exercise/src/app/header-component/header-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header-component.component.html',
+  styleUrl: './header-component.component.css'
+})
+export class HeaderComponentComponent {
+
+}
